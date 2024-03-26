2ec2fc32c0e90f8960fe912b9306aa387942af95
parveen-jangir <jangirparveen95@gmail.com>
2024-03-26 16:29:27 +0530

    Add OTA in ball Feeder code

--- Cricket Bowling Machine/Software/src/main.cpp
+++ Cricket Bowling Machine/Software/src/main.cpp
@@ -1,3 +1,14 @@
+// #include <WiFi.h>
+// #include <AsyncTCP.h>
+
+// #include <ESPAsyncWebServer.h>
+// #include <AsyncElegantOTA.h>
+
+// const char *ssid = "ECB_TEQIP";
+// const char *password = "";
+
+// AsyncWebServer server(80);
+
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -232,12 +243,38 @@ void mode()
         }
     }
 }
+
+// BY PARVEEN
+
 //------------------------------------------------------------------------------------//
 
 void setup()
 {
     Serial.begin(115200);
     SerialBT.begin(device_name);
+
+    // WiFi.mode(WIFI_STA);
+    // WiFi.begin(ssid, password);
+    // Serial.println("");
+
+    // // Wait for connection
+    // while (WiFi.status() != WL_CONNECTED)
+    // {
+    //     delay(500);
+    //     Serial.print(".");
+    // }
+    // Serial.println("");
+    // Serial.print("Connected to ");
+    // Serial.println(ssid);
+    // Serial.print("IP address: ");
+    // Serial.println(WiFi.localIP());
+
+    // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+    //           { request->send(200, "text/plain", "Hi! This is a sample response."); });
+
+    // AsyncElegantOTA.begin(&server); // Start AsyncElegantOTA
+    // server.begin();
+    // Serial.println("HTTP server started");
 
     //*****Push Buttons*****//
     pinMode(speed_Inc, INPUT);
